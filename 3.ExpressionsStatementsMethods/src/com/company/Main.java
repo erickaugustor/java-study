5d01ea0777ca2c5eeb773e392082a691333745ba
erick.augustor <erick.augustoramos@gmail.com>
2018-01-04 22:06:54 -0200

    method overloading

--- 3.ExpressionsStatementsMethods/src/com/company/Main.java
+++ 3.ExpressionsStatementsMethods/src/com/company/Main.java
@@ -57,26 +57,52 @@ public class Main {
         //Method Overloading
         //The same method name, with different parameters
 
-        int newScore = calculateScore("Tim", 500);
-        System.out.println("New score is " + newScore);
-        calculateScore(75);
-        calculateScore();
+//        int newScore = calculateScore("Tim", 500);
+//        System.out.println("New score is " + newScore);
+//        calculateScore(75);
+//        calculateScore();
+
+        calcFeetAndInchesToCentimeters(156);
+
     }
 
-    public static int calculateScore(String playerName, int score){
-        System.out.println("Player " + playerName + " scored " + score + " points");
-        return score * 1000;
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if((feet < 0) || (inches < 0) || (inches >12)){
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + "cm");
+        return centimeters;
     }
 
-    public static int calculateScore(int score){
-        System.out.println("Unnamed player scored " + score + " points");
-        return score * 1000;
-    }
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if(inches < 0){
+            return -1;
+        }
 
-    public static int calculateScore(){
-        System.out.println("No player name, no player score");
-        return 0;
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and = " + remainingInches );
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
     }
+//    public static int calculateScore(String playerName, int score){
+//        System.out.println("Player " + playerName + " scored " + score + " points");
+//        return score * 1000;
+//    }
+//
+//    public static int calculateScore(int score){
+//        System.out.println("Unnamed player scored " + score + " points");
+//        return score * 1000;
+//    }
+//
+//    public static int calculateScore(){
+//        System.out.println("No player name, no player score");
+//        return 0;
+//    }
 
 
 //    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
