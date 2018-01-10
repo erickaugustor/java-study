fe73f1420363e00b37f8c63c9d7b40d9bb5bfb99
erick.augustor <erick.augustoramos@gmail.com>
2018-01-10 00:13:20 -0200

    composition

--- 6.OOP/Composition/src/com/company/Main.java
+++ 6.OOP/Composition/src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("2208", "Dell", "248", dimensions);
+        //System.out.println(theCase.getDimensions().getDepth());
+
+        Monitor theMonitor = new Monitor("27inh Beast", "Acer", 27 , new Resolution(2548, 1440));
+
+        Motherboard theMotherboard = new Motherboard("B3-3434", "Asus", 4, 5, "v5.22");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.getMonitor().drawPixelArt(1500, 1200, "red");
     }
 }
