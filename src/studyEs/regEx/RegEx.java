9e396ad1fe127f36bded43f846fe221efb0ff9e3
erick.augustor <erick.augustoramos@gmail.com>
2018-06-07 12:23:53 -0300

    Aula POO - RegEx

---
+++ src/studyEs/regEx/RegEx.java
@@ -0,0 +1,11 @@
+package studyEs.regEx;
+
+public class RegEx {
+    // Expressões regulares surgem para validar as coisas.
+    // Linguagem ={4} termina com 4 digitos
+    // [0-9]
+    // antes vem o traço - ?9?  // 9 é opcional!
+    // (?:\\([0-9]{2}\\) )?
+    // o \\ é para sinlizar que isso é colocado na linguagem.
+
+}
